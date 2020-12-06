631732d30816c34f0d7aa5e8abffe92799da57ce
clementreiffers <reiffersclement@gmail.com>
2020-12-06 10:16:52 +0100

    code jauge contrainte ok

--- jauge_contrainte_micro/jauge_contrainte_micro.ino
+++ jauge_contrainte_micro/jauge_contrainte_micro.ino
@@ -1,34 +1,32 @@
+#ifndef cbi
+#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+#endif
+#ifndef sbi
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+#endif
+
+int analogReadNew(uint8_t pin)
+{
+  uint8_t low, high;
+  if (pin >= 14) pin -= 14; // allow for channel or pin numbers
+  ADMUX = (INTERNAL << 6) | (pin & 0x07);
+  sbi(ADCSRA, ADSC);
+  // ADSC is cleared when the conversion finishes
+  while ((_SFR_BYTE(ADCSRA) & _BV(ADSC))); // bit is set
+  low  = ADCL;
+  high = ADCH;
+  // combine the two bytes
+  return (high << 8) | low;
+}
+
+
 void setup() {
-  Serial.begin(9600);  
-// Désactivons l'ADC pour l'instant
-  ADCSRA &= B01111111; 
-  ADCSRB=0x00;
-  ADMUX|=0&0x07;
-  ADMUX|= (1<< REFS0);
-  ADMUX|= (1<< REFS1);
-  ADCSRA &= B11111011;
-  ADCSRA |= B00000010;
-  ADCSRA |= B00000001;
-  ADCSRA |= B00100000;
-  ADCSRA |= B00001000;
-  ADCSRA |= B10000000;
-  ADCSRA |= B01000000;
-  sei();
+  Serial.begin(9600);
 }
-
-void loop(){
-//Code à exécuter lors d'une mesure analogique
-//analogReference(INTERNAL);
-  int valeur_brute = (ADCH<<8) | ADCL;
-  float temperature_celcius = valeur_brute * (1.1 / 1023.0 * 100.0);
-//Serial.println(temperature_celcius);
-  delay(200);
-}
-
-ISR(ADC_vect){
-  // Code à exécuter lors d'une mesure analogique3
-  int valeur_brute= (ADCH<<8) | ADCL;
-  float temperature_celcius = valeur_brute * (1.1 / 1023.0 * 100.0);
-  Serial.println(temperature_celcius);
-  delay(200);
+void loop() {
+  // ne pas toucher à ces calculs !!!
+  float valeur = analogReadNew(A0);
+  valeur = valeur * 780;
+  valeur = valeur / 1023;
+  Serial.println(valeur);
 }
